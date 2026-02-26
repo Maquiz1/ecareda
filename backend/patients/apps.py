687c77ed14fddee469c6b87b47170c57ed3171ab
Maquiz1 <manquiz92@gmail.com>
2026-02-26 13:02:33 +0300

    first commit

--- backend/patients/apps.py
+++ backend/patients/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
 
+# class PatientsConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'patients'
+    
 class PatientsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'patients'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "patients"
+    verbose_name = "Patients & Subjects"
