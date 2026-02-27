8b93d9c6b57327c857bbb226c27ae414c38016e7
Maquiz1 <manquiz92@gmail.com>
2026-02-27 23:25:09 +0300

    fix

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+SECRET_KEY = env('ALLOWED_HOSTS')
 
 
 # Application definition
