bdc735adb39ed233080aafc230b015bf2702ded5
Maquiz1 <manquiz92@gmail.com>
2026-03-04 17:31:45 +0300

    fix

--- backend/accounts/models/users/user_model.py
+++ backend/accounts/models/users/user_model.py
@@ -1,16 +1,32 @@
-# from django.contrib.auth.models import AbstractUser
-# from django.db import models
-# from organizations.models import Organization
+# accounts/models/users/user_model.py
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from organizations.models import Organization
+from sites.models import Site
 
 
-# class User(AbstractUser):
+class User(AbstractUser):
 
-#     organization = models.ForeignKey(
-#         Organization,
-#         on_delete=models.SET_NULL,
-#         null=True,
-#         blank=True
-#     )
+    organization = models.ForeignKey(
+        Organization,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
 
-#     def __str__(self):
-#         return self.username
+    site = models.ForeignKey(
+        Site,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    # EDC roles
+    is_data_manager = models.BooleanField(default=False)
+    is_monitor = models.BooleanField(default=False)
+    is_pi = models.BooleanField(default=False)
+    is_coordinator = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
