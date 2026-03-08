a4b9279924582ebd75feefa9311fc0a8298ef28a
Maquiz1 <manquiz92@gmail.com>
2026-03-08 16:48:53 +0300

    fix

--- backend/subjects/views/subject_list_view.py
+++ backend/subjects/views/subject_list_view.py
@@ -2,11 +2,16 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from subjects.models import Subject
 
+
 class SubjectListView(LoginRequiredMixin, ListView):
     model = Subject
     template_name = "subjects/subject_list.html"
     context_object_name = "subjects"
 
     def get_queryset(self):
-        return Subject.objects.filter(is_deleted=False)
-
+        return (
+            Subject.objects
+            .filter(is_deleted=False)
+            .select_related("site", "project")
+            .order_by("site__name", "subject_id")
+        )
