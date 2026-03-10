8ef2cfac5a0686e0cbc477719f43486ad00d0192
Maquiz1 <manquiz92@gmail.com>
2026-03-11 01:20:48 +0300

    fix all

--- backend/organizations/models/organizations_model.py
+++ backend/organizations/models/organizations_model.py
@@ -1,7 +1,6 @@
 from django.db import models
 # from core.models import ActiveModel
 
-
 class Organization(models.Model):
     name = models.CharField(max_length=255)
     code = models.CharField(max_length=50, unique=True)
