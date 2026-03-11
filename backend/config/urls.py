4646aae53c1e1dd543a48deb76c96e061354601d
Maquiz1 <manquiz92@gmail.com>
2026-03-11 17:16:20 +0300

    Fix CSS FOLDER

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,6 +4,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.views.generic import RedirectView
 
+
+SITE_TITLE = "e-Careda Administarions"
+SITE_TITLE = "e-Careda Administarions"
+
+
 urlpatterns = [
     path("", RedirectView.as_view(url="/dashboard/")),
     path("dashboard/", include("dashboard.urls")),
