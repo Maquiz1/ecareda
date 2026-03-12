1142e3a4bce3a407f8431dfebce463bee0e3d408
Maquiz1 <manquiz92@gmail.com>
2026-03-12 09:40:28 +0300

    Finalize Base Template

--- backend/organizations/models/organizations_model.py
+++ backend/organizations/models/organizations_model.py
@@ -4,6 +4,7 @@ from django.db import models
 class Organization(models.Model):
     name = models.CharField(max_length=255)
     code = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
